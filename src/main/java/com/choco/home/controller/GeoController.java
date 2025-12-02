8e310a4668d5030092b3e1cef4242a11bd69eb92
Sreeram <116187896+print-Ram@users.noreply.github.com>
2025-12-03 01:02:37 +0530

    Update Google API key property in GeoController

--- src/main/java/com/choco/home/controller/GeoController.java
+++ src/main/java/com/choco/home/controller/GeoController.java
@@ -11,7 +11,7 @@ import java.nio.charset.StandardCharsets;
 @RequestMapping("/api/geo")
 public class GeoController {
 
-    @Value("${google.api.key}")
+    @Value("${google.maps.api.key}")
     private String googleApiKey;
 
     @GetMapping("/search")
